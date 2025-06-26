a4ad86ad62d19bff1cdbba144dbf263c0caea97d
Cargarort3 <cargarort3@alum.us.es>
2025-06-26 19:35:19 +0200

    feat(#23): add publication solution

--- app/publication/models.py
+++ app/publication/models.py
@@ -5,7 +5,7 @@ from datetime import datetime, timezone
 class Publication(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     posted_at = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
-    max_movements = db.Column(db.Integer)
+    min_movements = db.Column(db.Integer)
 
     labyrinth_id = db.Column(db.Integer, db.ForeignKey('labyrinth.id'), nullable=False, unique=True)
     labyrinth = db.relationship('Labyrinth', lazy=True, uselist=False)
